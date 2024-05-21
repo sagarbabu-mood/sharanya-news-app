b6354314cb86d8ad42566b461405227286d54915
sanjana <sanjana.arjun12@gmail.com>
2024-05-21 11:49:58 +0000

    newsapp

---
+++ src/components/Home/index.js
@@ -0,0 +1,146 @@
+import React, {Component} from 'react'
+import {MdHome} from 'react-icons/md'
+import {MdOutlineExplore} from 'react-icons/md'
+import {FaBookmark} from 'react-icons/fa'
+import {CgProfile} from 'react-icons/cg'
+import {Link, withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import ProductCard from '../ProductCard'
+import Header from '../Header'
+import './index.css'
+
+class Home extends Component {
+  state = {
+    news: [],
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
+    if (response.ok) {
+      const fetchedData = await response.json()
+      console.log(fetchedData)
+      const updatedData = fetchedData.products.map(product => ({
+        title: product.title,
+        brand: product.brand,
+        price: product.price,
+        id: product.id,
+        imageUrl: product.image_url,
+        rating: product.rating,
+      }))
+      this.setState({
+        news: updatedData,
+      })
+    }
+  }
+
+  onClickProfile = () => {
+    return
+  }
+
+  render() {
+    const {news} = this.state
+    return (
+      <>
+        <Header />
+        <div className="home-container">
+          <input type="search" className="searchcontainer" />
+          <div className="trendingupper">
+            <p className="trending">Trending</p>
+            <a href="https://www.bbc.com/news/world" target="_blank">
+              see all
+            </a>
+          </div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
+            className="trendingimage"
+          />
+          <div className="trendingupper">
+            <p className="trending">Latest</p>
+            <a href="https://www.bbc.com/news/world" target="_blank">
+              see all
+            </a>
+          </div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to={news.brand}
+              >
+                All
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to={'news.brand'}
+              >
+                MAJIK
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={news.brand}>
+                LEVIS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={news.brand}>
+                Mufti
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={news.brand}>
+                Nova
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={news.brand}>
+                Amazon
+              </Link>
+            </li>
+          </ul>
+          <ul className="products-list">
+            {news.map(product => (
+              <ProductCard productData={product} key={product.id} />
+            ))}
+          </ul>
+          <footer className="footersec">
+            <div className="footercontainers">
+              <MdHome size="20" />
+              <p>Home</p>
+            </div>
+            <div className="footercontainers">
+              <MdOutlineExplore size="20" />
+              <p>Explore</p>
+            </div>
+            <div className="footercontainers">
+              <FaBookmark size="20" />
+              <p>BookMark</p>
+            </div>
+            <div className="footercontainers">
+              <CgProfile size="20" />
+              <p onClick={this.onClickProfile} type="button">
+                Profile
+              </p>
+            </div>
+          </footer>
+        </div>
+      </>
+    )
+  }
+}
+
+export default withRouter(Home)
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,59 @@
+import './index.css'
+
+const onClickNext = event => {
+  event.preventdefault
+}
+const NotFound = () => {
+  return (
+    <div>
+      <form className="not-found-container" onSubmit={onClickNext}>
+        <input type="file" />
+        <div>
+          <label for="username" className="labelel">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            className="inputel"
+            name="username"
+            required
+          />
+        </div>
+        <div>
+          <label for="name" className="labelel">
+            Full Name*
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="inputel"
+            name="fullname"
+            required
+          />
+        </div>
+        <div>
+          <label for="email" className="labelel">
+            Email Address*
+          </label>
+          <input
+            type="text"
+            id="email"
+            className="inputel"
+            name="email"
+            required
+          />
+        </div>
+        <div>
+          <label for="num" className="labelel">
+            Phone Number*
+          </label>
+          <input type="text" id="num" className="inputel" name="num" required />
+        </div>
+        <button className="buttonelement">Next</button>
+      </form>
+    </div>
+  )
+}
+
+export default NotFound
